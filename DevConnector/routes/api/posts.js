da167160929ade06fa9fff1b1a4c8b30ff9d4c96
Vladi Iokhim <mac@macs-MacBook-Pro.local>
2019-10-21 21:12:34 +0300

    Section 5:  Add & Remove Comment Routes

--- DevConnector/routes/api/posts.js
+++ DevConnector/routes/api/posts.js
@@ -108,7 +108,7 @@ const putUnLikePostController = async (req,res,next) => {
         }
         //Get remove index
         const removeIndex = post.likes.map(like=> like.user.toString()).indexOf(req.user.id);
-       post.likes.splice(removeIndex,1);
+        post.likes.splice(removeIndex,1);
         await post.save();
         res.json(post.likes);
 
@@ -117,10 +117,61 @@ const putUnLikePostController = async (req,res,next) => {
         res.status(500).send('Server Error'); 
     }
 };   
+const createCommentPostController = async (req,res,next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password'),
+              post = await Post.findById(req.params.id),
+        NewComment = {
+            text : req.body.text,
+            name : user.name,
+            avatar : user.avatar,
+            user : req.user.id
+        };  
+        post.comments.unshift(NewComment);
+        await post.save();
+        res.json(post.comments);
 
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+};
+
+const deleteCommentPostController = async (req,res,next) => {
+    try {
+        const post  = await Post.findById(req.params.post_id);
+        //Pull out comment from the post
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+        //Make sure comment exists
+        if(!comment){
+            return res.status(404).json({msg:'Comment Does not Exists'});
+        }
+        //Check User
+        if(comment.user.toString() !== req.user.id){
+            return res.status(404).json({msg:'User Not Authorized'});
+        }
+        //Get remove index
+        const removeIndex = post.comments.map(comment=> comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex,1);
+        await post.save();
+        res.json(post.comments);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    }
+};
 
 //Validators
 const createPostValidator = [
+    check('text','Text Is Required').not().isEmpty()
+];
+const createCommentPostValidator = [
     check('text','Text Is Required').not().isEmpty()
 ];
 
@@ -170,6 +221,21 @@ router.put('/like/:id',auth,putLikePostController);
  * @access      Private
  */
 router.put('/unlike/:id',auth,putUnLikePostController);
+
+/**
+ * @route       POST api/posts/comment/:id
+ * @description comment on a post
+ * @access      Private
+ */
+router.post('/comment/:id',[auth,createCommentPostValidator],createCommentPostController);
+
+
+/**
+ * @route       DELETE api/posts/comment/:post_id/:comment_id
+ * @description delete a comment from  post
+ * @access      Private
+ */
+router.delete('/comment/:post_id/:comment_id',auth,deleteCommentPostController);
 
 
 
