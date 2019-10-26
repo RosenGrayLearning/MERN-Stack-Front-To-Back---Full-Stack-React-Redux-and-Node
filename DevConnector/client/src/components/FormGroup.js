a8f0952ed7379a1b71e7073b618e86ec2930870f
Vladi Iokhim <mac@macs-MacBook-Pro.local>
2019-10-26 16:23:56 +0300

    Section 6:Getting Started With React & The Frontend-Register Form & useState Hook

---
+++ DevConnector/client/src/components/FormGroup.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function FormGroup(props) {
+    return (
+        <div className="form-group"> <input type="text" {...props}/>
+      </div>
+    )
+}
--- DevConnector/client/src/components/auth/Register.js
+++ DevConnector/client/src/components/auth/Register.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React,{Fragment,useState} from 'react'
+import FormGroup from './../FormGroup';
 
 const Register = () => {
+    const [formData,setFormData] = useState({
+        name:'',
+        email:'',
+        password:'',
+        password2 : ''
+
+    });
+    const {name,email,password,password2} = formData;
+    const formGroupProps = [
+        {type:'text',placeholder:'Name',value:name,name:'name'},
+        {type:'email',placeholder:'Email Address',value:email,name:'email'},
+        {type:'password',placeholder:'Password',value:password,name:'password'},
+        {type:'password',placeholder:'Confirm Password',value:password2,name:'password2'},
+];
+
+    const onChangeValue = e => {setFormData({...formData,[e.target.name]:e.target.value})};
+    const onSubmitForm = e => {
+        e.preventDefault();
+        if(password !== password2){
+            console.log('Passwords do not match!');
+        }else{
+            console.log(formData);
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+
+    <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+        <form className="form" onSubmit={onSubmitForm}>
+            {
+           formGroupProps.map(formGroup=><FormGroup required onChange={onChangeValue} key={formGroup.name} {...formGroup}/>)     
+            }
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <a href="login.html">Sign In</a>
+        </p>
+        </Fragment>
     )
 }
 
